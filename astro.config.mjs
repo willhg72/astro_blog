1a4453b56d11cd3ef91328e4ef28ff9695506c2b
William Hernández. <51140355+willhg72@users.noreply.github.com>
2025-03-09 18:49:17 -0500

    chore(git): remove .vercel files from staging and update .gitignore
    - Unstage the .vercel folder from the repository.
    - Add .vercel/ to .gitignore to prevent future commits of Vercel configuration files.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://sistemas-operativos-blog.vercel.app', // <--- AÑADE ESTO
   // Indica que quieres SSR en lugar de estático
   output: 'server',
 
